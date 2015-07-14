5e219d6398c755bff7bd2e9d2091d8fc38d8c3c5
Alexander Eckers <gagagu@web.de>
2015-07-14 09:19:53 +0200

    cleanup repository

--- Gagagu VR Streamer Server/bin/Debug/shader/compile_shader.cmd
+++
@@ -1,4 +0,0 @@
-"C:\\Program Files (x86)\\Microsoft DirectX SDK (June 2010)\\Utilities\\bin\\x64\\fxc.exe" /T ps_2_0 /E main /Fo"Shader.ps" "Shader.fx"
-pause
-"C:\\Program Files (x86)\\Microsoft DirectX SDK (June 2010)\\Utilities\\bin\\x64\\fxc.exe" /T ps_2_0 /E main /Fo"Shader2.ps" "Shader2.fx"
-pause
